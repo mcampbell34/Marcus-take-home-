3bc634f25fe031ed71348ca5c263a384a30d415f
Marcus Brown <marcus@marcus.attlocal.net>
2021-12-23 18:07:37 -0800

    Polishing up answers

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
 }
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/search',
+        destination: ' https://vercel.com',
+        permanent: true,
+      },
+    ]
+  },
+}
