f5372dc3136200929d596352316c1eee73aece56
Marcus Brown <marcus@marcus.attlocal.net>
2021-12-23 18:32:20 -0800

    Polishing up answers

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ module.exports = {
 {
   "redirects"; [
     { 
-      "source": "/search",
+      "source": "/Users/mb/Desktop/Take%20Home%20Assignment%20/Marcus-take-home-/search",
       "destination": "https://vercel.com", 
       "permanent": false
     }
