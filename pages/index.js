babd53811299178ebd8b8132260588d21b14bbd8
Marcus Campbell <48104204+mcampbell34@users.noreply.github.com>
2021-12-23 16:05:38 -0800

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -34,7 +34,7 @@ export default function Home() {
                 
                  My top 5 least faviorate task included:
                 - Help resolve billing issues for customers
-                - Dig through logs to troubleshoot a customer's broken project
+                - Dig through logs to troubleshoot a customers broken project
                 - Respond to 50+ support requests via email every day </p>
           </div>
 
@@ -44,7 +44,7 @@ export default function Home() {
               I plan on doing this by taking advange of the learning fund to enroll in Udemy cources that are centered
                 around these two lanages. Something that I am passionate about is supporting my teammates when it comes to 
                   understanding the techincal networking aspects of the web such as how to use tools such a cURL, mtr, and even dig to 
-                    look deeper into a customer's issue and provide the appropiate guidence to get them the expected behavior they are looking for.</p>
+                    look deeper into a customers issue and provide the appropiate guidence to get them the expected behavior they are looking for.</p>
           </a>
 
           <a
@@ -52,16 +52,16 @@ export default function Home() {
             className={styles.card}
           >
             <h2>Question 3e</h2>
-            <p>One way to redirect you can add the following within the "next.config.js" file if you are using Next.js or within the "vercel.json" 
+            <p>One way to redirect you can add the following within the next.config.js file if you are using Next.js or within the vercel.json 
             (https://vercel.com/support/articles/does-vercel-support-permanent-redirects?query=redire#in-application-redirects). If you are using something else
-             By following this guide, you will see a "308" response code on your request. You can use this command to test this redirect behavior, "curl -svo dev/null https://vercel-take-home-mcampbell34.vercel.app/search"
+             By following this guide, you will see a 308 response code on your request. You can use this command to test this redirect behavior, curl -svo dev/null https://vercel-take-home-mcampbell34.vercel.app/search
              </p>
          
 
            <h2>Question 3f</h2>
             <p>
               To use your GoDaddy domain with Vercel, you can follow these steps: https://vercel.com/guides/transferring-domains-to-vercel. 
-              Please let me know if you have any specific questions regarding this process and I'll be more than happy to further assist. 
+              Please let me know if you have any specific questions regarding this process and I will be more than happy to further assist. 
             </p>
           
            <a
@@ -73,7 +73,7 @@ export default function Home() {
               One common issue that I can forsee customers at Vercel running into would be around DNS configuration. At times, DNS will cache
               results and even though a customer made changes that are correct, they may still need to wait for the TTL of their DNC configrations to 
               flush before the new changes are set. I would focus on helping our customer understand how DNS works and the tolls avalible they can use to 
-              debug these issues on their end such as "dig". 
+              debug these issues on their end such as dig. 
             </p> 
             </a> 
 
@@ -84,7 +84,7 @@ export default function Home() {
            <h2>Question 3h</h2>
             <p>
               This process overall was great, very straight forward and to the point and I had fun brushing up on my Git and Github skills. I now I will 
-              need to put aside sometime to get back into using the git command line as I haven't used it since attending Lambda but, I was able to find
+              need to put aside sometime to get back into using the git command line as I have not used it since attending Lambda but, I was able to find
               alernatives to get the task done. 
             </p>
           </a>
